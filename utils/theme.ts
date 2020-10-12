dc9ac72b2b411c4b75ea47640adb1affacb9c662
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2020-10-12 00:20:21 -0400

    Update theme.ts

--- utils/theme.ts
+++ utils/theme.ts
@@ -2,7 +2,7 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import { orange } from "@horizon/colors/primary";
 import grays from "@horizon/colors/grays";
 import { typographyStyles } from "@horizon/typography";
-
+// testing
 export default createMuiTheme({
   palette: {
     primary: { ...orange, contrastText: grays.white },
